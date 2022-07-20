757bd0bc657aae2ffb63a62e21cf1a1f5aee307d
alvinsss <6449694@qq.com>
2022-07-20 09:01:31 +0800

    JSONPath获取返回的token

--- apitest/src/test/java/com/alvin/cases/LoginCase.java
+++ apitest/src/test/java/com/alvin/cases/LoginCase.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.alibaba.fastjson.JSONPath;
 import com.alvin.pojo.API;
 import com.alvin.pojo.Case;
 import com.alvin.utils.ExcelUtils;
@@ -13,6 +14,13 @@ import com.alvin.utils.HttpUtils;
 
 public class LoginCase {
 	
+	public static void main(String[] args) {
+		String json="{\"code\":0,\"msg\":\"OK\",\"data\":{\"id\":15290,\"leave_amount\":0.0,\"mobile_phone\":\"13221400113\",\"reg_name\":\"qatest\",\"reg_time\":\"2022-07-18 10:14:01.0\",\"type\":1,\"token_info\":{\"token_type\":\"Bearer\",\"expires_in\":\"2022-07-20 09:03:45\",\"token\":\"eyJhbGciOiJIUzUxMiJ9.eyJtZW1iZXJfaWQiOjE1MjkwLCJleHAiOjE2NTgyNzkwMjV9.7EQOwY_KrAs5JQq5WQvxibtj7JOR2PNEFJ4Y0qSAeMdMSPvwRIWOr-35fT_YNe07T4O0sOXnQm7fHzq_666NOQ\"}},\"copyright\":\"Copyright 柠檬班 © 2017-2019 湖南省零檬信息技术有限公司 All Rights Reserved\"}";
+		Object read = JSONPath.read(json, "$.data.token_info.token");
+		Object read2 = JSONPath.read(json, "$..token");
+		System.out.println(read2);
+
+	}
 	
 	/**
 	 * 注册用例的测试方法
@@ -26,7 +34,7 @@ public class LoginCase {
 //		1、参数化替换
 //		2、数据库前置查询结果(数据断言必须在接口执行前后都查询)
 //		3、调用接口
-		HttpUtils.call(api.getUrl(), api.getMethod(), c.getParams(), api.getContentType());
+		String body=HttpUtils.call(api.getUrl(), api.getMethod(), c.getParams(), api.getContentType());
 //		4、断言响应结果
 //		5、添加接口响应回写内容
 //		6、数据库后置查询结果
