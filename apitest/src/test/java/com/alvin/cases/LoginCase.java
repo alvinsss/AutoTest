120d5e8d72ed07d0a3033e117ad71069ca6b495b
alvinsss <6449694@qq.com>
2022-07-20 09:27:27 +0800

    配置充值测试用例

--- apitest/src/test/java/com/alvin/cases/LoginCase.java
+++ apitest/src/test/java/com/alvin/cases/LoginCase.java
@@ -2,6 +2,7 @@ package com.alvin.cases;
 
 import java.util.Arrays;
 
+import org.bouncycastle.asn1.cms.EnvelopedData;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ import org.testng.annotations.Test;
 import com.alibaba.fastjson.JSONPath;
 import com.alvin.pojo.API;
 import com.alvin.pojo.Case;
+import com.alvin.utils.EnvironmentUtils;
 import com.alvin.utils.ExcelUtils;
 import com.alvin.utils.HttpUtils;
 
@@ -35,6 +37,17 @@ public class LoginCase {
 //		2、数据库前置查询结果(数据断言必须在接口执行前后都查询)
 //		3、调用接口
 		String body=HttpUtils.call(api.getUrl(), api.getMethod(), c.getParams(), api.getContentType());
+		Object token = JSONPath.read(body, "$.data.token_info.token");
+
+		// 防止只有member_id没有token值
+		if (token != null) {
+			EnvironmentUtils.env.put("${token}",token.toString());
+			Object member_Id = JSONPath.read(body, "$.data.id");
+			if (member_Id != null) {
+				EnvironmentUtils.env.put("${member_id}",member_Id.toString());
+			}
+		}
+
 //		4、断言响应结果
 //		5、添加接口响应回写内容
 //		6、数据库后置查询结果
---
+++ apitest/src/test/java/com/alvin/cases/RechargeCase.java
@@ -0,0 +1,52 @@
+package com.alvin.cases;
+
+import java.util.Arrays;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.alvin.pojo.API;
+import com.alvin.pojo.Case;
+import com.alvin.utils.ExcelUtils;
+import com.alvin.utils.HttpUtils;
+
+public class RechargeCase {
+	
+	
+	/**
+	 * 注册用例的测试方法
+	 * @param url			接口请求地址
+	 * @param method		接口请求方法
+	 * @param params		接口请求参数
+	 * @param contentType	接口类型
+	 */
+	@Test(dataProvider = "datas")
+	public void test(API api,Case c) {
+//		1、参数化替换
+//		2、数据库前置查询结果(数据断言必须在接口执行前后都查询)
+//		3、调用接口
+		HttpUtils.call(api.getUrl(), api.getMethod(), c.getParams(), api.getContentType());
+//		4、断言响应结果
+//		5、添加接口响应回写内容
+//		6、数据库后置查询结果
+//		7、据库断言
+//		8、添加断言回写内容
+//		9、添加日志
+//		10、报表断言
+	}
+
+	
+	@DataProvider
+	public Object[][] datas() {
+//		Object[][] datas = ExcelUtils.read();
+//		return datas;
+		System.out.println("RechargeCase datas:");
+		Object[][] datas = ExcelUtils.getAPIAndCaseByApiId("3");
+		for (Object[] objects : datas) {
+			System.out.println(Arrays.toString(objects));
+		}
+		return datas;
+	}
+	
+	
+}
