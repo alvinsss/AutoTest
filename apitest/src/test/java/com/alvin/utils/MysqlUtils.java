464739a76dc3acf1ff78621b6dfbfd029f7d4ce7
alvinsss <6449694@qq.com>
2022-07-21 11:26:48 +0800

    getSQLSingleReuslt 根据sql语句执行查询单个结果集

--- apitest/src/test/java/com/alvin/utils/MysqlUtils.java
+++ apitest/src/test/java/com/alvin/utils/MysqlUtils.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ArrayHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.commons.lang3.StringUtils;
 
 import com.alvin.pojo.Member;
 
@@ -14,13 +16,39 @@ public class MysqlUtils {
 	
 	
 	public static void main(String[] args) throws Exception {
-		queryBeanHander();
+		getSQLSingleReuslt("SELECT  name from member WHERE id=1");
 	}
 
-	private static void queryObjects() throws SQLException {
+	/**
+	 * 	根据sql语句执行查询单个结果集
+	 * @param sql	sql语句
+	 * @return
+	 */
+	public static Object getSQLSingleReuslt(String sql) {
+		//1、如果sql语句为空，不执行sql查询
+		if(StringUtils.isBlank(sql)) {
+			return null;
+		}
+		//2、DBUtils操作sql语句核心类
+		QueryRunner qr = new QueryRunner();
+		//3、获取数据库连接
+		Connection conn = JDBCUtils.getConnection();
+		//3.1、定义返回值
+		Object result = null;
+		try {
+			//4、执行sql语句
+			result = qr.query(conn,sql,new ScalarHandler<Object>());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(result);
+		return result;
+	}
+	
+	private static void getqueryArrayHandler(String sql) throws SQLException {
 		QueryRunner qr = new QueryRunner();
 		Connection conn = JDBCUtils.getConnection();
-		Object[] objects = qr.query(conn,"select * from xinxibiao x WHERE x.xuhao='1';", new ArrayHandler());
+		Object[] objects = qr.query(conn,sql, new ArrayHandler());
 	
 		for (Object object : objects) {
 			System.out.println(object);
@@ -28,12 +56,11 @@ public class MysqlUtils {
 		
 		JDBCUtils.close(conn);
 	}
-	
-	private static void queryBeanHander() throws SQLException {
+	private static void getqueryBeanHander(String sql) throws SQLException {
 		QueryRunner qr = new QueryRunner();
 		Connection conn = JDBCUtils.getConnection();
 		//new BeanHandler<Member>(Member.class) 结果及处理对象，使用BeanHandler需要pojo创建实体类对象
-		Member member = qr.query(conn,"select * from member m WHERE m.id=1 ;", new BeanHandler<Member>(Member.class));
+		Member member = qr.query(conn,sql, new BeanHandler<Member>(Member.class));
 		System.out.println(member);
 		JDBCUtils.close(conn);
 	}
