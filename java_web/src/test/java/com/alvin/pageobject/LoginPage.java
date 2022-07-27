2e86e6c97de9b6a389a8bb76e32c3b93793bae0a
alvinsss <6449694@qq.com>
2022-07-27 16:30:08 +0800

    PO模式封装loginpage

---
+++ java_web/src/test/java/com/alvin/pageobject/LoginPage.java
@@ -0,0 +1,31 @@
+
+package com.alvin.pageobject;
+
+import org.openqa.selenium.By;
+
+import com.alvin.testcases.LoginTest;
+
+/**
+* @Title: LoginPage 
+* @Description:  登录页面对象
+* @author: alvin
+* @date 2022年7月27日 下午4:23:18
+*/
+public class LoginPage {
+	
+	private  By useraccountBy = By.name("account");
+	private  By passwordBy = By.name("password");
+	private  By submitBy = By.id("submit");
+	
+	public void inputUserAccount(String account) {
+		LoginTest.driver.findElement(useraccountBy).sendKeys(account);
+	}
+	
+	public void inputPassword(String password) {
+		LoginTest.driver.findElement(passwordBy).sendKeys(password);
+	}
+	
+	public void clickLoginButton( ) {
+		LoginTest.driver.findElement(submitBy).click();
+	}
+}
