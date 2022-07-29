0d2672c9c97beb4c070f2f51e3a926419f5b8b18
alvinsss <6449694@qq.com>
2022-07-29 10:54:54 +0800

    LoginPage做元素和操作封装，LoginFlow做操作集合的封装，LoginTest做测试数据的循环调用执行和断言

--- java_web/src/test/java/com/alvin/testcases/LoginTest.java
+++ java_web/src/test/java/com/alvin/testcases/LoginTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.log.Log;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,12 +20,13 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.alvin.businessflow.LoginFlow;
 import com.alvin.pageobject.IndexPage;
 import com.alvin.pageobject.LoginPage;
+import com.alvin.testdata.Constant;
 import com.alvin.testdata.LoginDatas;
 import com.alvin.util.BrowserUtil;
-import com.alvin.util.Constant;
- 
+  
 /**
 * @Title: LoginTest
 * @Description: 
@@ -44,45 +46,60 @@ public class LoginTest {
 		BrowserUtil.driver.manage().window().maximize();
 	}
 
-	@Test
+	@Test(description = "正确的account ,password的用例")
 	public void login_asuccess() throws Exception {
 		System.out.println("test login_success");
-		LoginPage loginPage = new LoginPage();
-		IndexPage indexPage = new IndexPage();
-		loginPage.inputUserAccount(LoginDatas.CORRECT_ACCOUNT);
-		loginPage.inputPassword(LoginDatas.CORRECT_PASSWORD);
-		loginPage.clickLoginButton();
+		System.out.println(LoginDatas.CORRECT_ACCOUNT+"---->"+LoginDatas.CORRECT_PASSWORD);
+		LoginFlow loginAction = new LoginFlow(LoginDatas.CORRECT_ACCOUNT,LoginDatas.CORRECT_PASSWORD);
+		loginAction.login();
 		Thread.sleep(1000);
 		//断言1 判断url地址
 		String expectedValue=Constant.INDEX_URL;
 		String actualValue=BrowserUtil.driver.getCurrentUrl();
+		System.out.println("actualValue:" + actualValue);
 		Assert.assertEquals(actualValue, expectedValue);
 		
 		//断言2 判断登录之后是否有退出按钮
+		IndexPage indexPage=new IndexPage();
 		Assert.assertTrue(indexPage.isDisplayed_ButtonQuit());
 		indexPage.buttonQuit();
 	}
 	
 	/**
-	 *  dataProvider注解返回2维数组，可以多次执行不同情况的测试
+	 *  dataProvider注解返回2维数组，根据dataProviderClass.dataProvider的方法名指定数据
 	 * @throws Exception
 	 */
 	@Test(description = "", dataProviderClass = LoginDatas.class, dataProvider = "getLoginFailureDatas")
-	public void login_dfailure_01(String account,String password, String expectedValue) throws Exception {
-		System.out.println("test login_failure");
-		System.out.println("account:"+account+"   password:"+password+"  expect:"+expectedValue);
-		LoginPage loginPage = new LoginPage();
-		loginPage.inputUserAccount(account);
-		loginPage.inputPassword(password);
-		loginPage.clickLoginButton();
+	public void login_dfailure_getLoginFailureDatas(String account,String password, String expectedValue) throws Exception {
+		LoginFlow loginAction = new LoginFlow(account,password);
+		loginAction.login();
 		Thread.sleep(1000);
 		//断言1 alert文本是否与预期一致
+		LoginPage loginPage = new LoginPage();
  		String actualValue=loginPage.getAlertText();
-		Assert.assertEquals(actualValue, "登录失败，请检查您的用户名或密码是否填写正确。");
+		Assert.assertEquals(actualValue, expectedValue);
+		Thread.sleep(1000);
 		loginPage.click_AlertDismiss();
 	}
 	
+	@Test(description = "", dataProviderClass = LoginDatas.class, dataProvider = "getLoginErrorDatas")
+	public void login_dfailure_getLoginErrorDatas(String account,String password, String expectedValue) throws Exception {
+		System.out.println("account:"+account+"   password:"+password+"  expect:"+expectedValue);
 
+		LoginFlow loginAction = new LoginFlow(account,password);
+		loginAction.login();
+		Thread.sleep(1000);
+		
+		//断言1 alert文本是否与预期一致
+		LoginPage loginPage = new LoginPage();
+ 		String actualValue=loginPage.getAlertText();
+ 		System.out.println("actualValue"+actualValue);
+ 		System.out.println("expectedValue"+expectedValue);
+		Assert.assertEquals(actualValue, expectedValue);
+		Thread.sleep(1000);
+		loginPage.click_AlertDismiss();
+	}
+	
 		
 	@AfterTest
 	public void tearDown() {
