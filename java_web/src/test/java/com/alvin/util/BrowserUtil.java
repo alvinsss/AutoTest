1e80d2453636ed441de2c8040a9c6ee0e5b9c19a
alvinsss <6449694@qq.com>
2022-07-29 16:06:49 +0800

    日志加入

--- java_web/src/test/java/com/alvin/util/BrowserUtil.java
+++ java_web/src/test/java/com/alvin/util/BrowserUtil.java
@@ -3,10 +3,13 @@ package com.alvin.util;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.alvin.base.BasePage;
 
 /**
 * @Title: BrowserUtil
@@ -15,6 +18,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 * @date 2022年7月27日 下午4:42:57
 */
 public class BrowserUtil {
+	
+	private static Logger logger = Logger.getLogger(BrowserUtil.class);
+
 	
 	public static WebDriver driver;
 	
@@ -27,21 +33,36 @@ public class BrowserUtil {
 		if ("chrome".equals(browserName)) {
 			ChromeDriver chromedriver = new ChromeDriver();
 			driver = chromedriver;
+			logger.info("打开chrome浏览器");
 		} else if ("firefox".equals(browserName)) {
 			System.setProperty("webdriver.firefox.bin", "D:\\UserTools\\Firefox\\firefox.exe");
 			System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
 			FirefoxDriver firefoxdriver = new FirefoxDriver();
 			driver = firefoxdriver;
-
+			logger.info("打开firefox浏览器");
 		} else if ("edge".equals(browserName)) {
 			System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
 			EdgeDriver edgedriver = new EdgeDriver();
 			driver = edgedriver;
+			logger.info("打开edge浏览器");
 		}
 	}
 	
- 
-	public static  void switchWindow(WebDriver driver,String title) {
+	
+	/**
+	 * 浏览器最大化
+	 */
+	public static void browserMaximize( ) {
+		driver.manage().window().maximize();
+	}
+	
+	
+	/**
+	 * 窗口切换
+	 * @param driver
+	 * @param title
+	 */
+	public static  void switchWindow(String title) {
 		//Set集合是无序 不能重复 不能保证第1个数据是第一个窗口
 		Set<String> handles = driver.getWindowHandles();
 		System.out.println(handles);
