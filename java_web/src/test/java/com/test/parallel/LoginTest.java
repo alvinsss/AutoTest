4323fa73df9cfdea9044d75f83a0c7fad9ff82ae
alvinsss <6449694@qq.com>
2022-08-01 11:13:44 +0800

    多线程testng执行demo

---
+++ java_web/src/test/java/com/test/parallel/LoginTest.java
@@ -0,0 +1,40 @@
+package com.test.parallel;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+/**
+* @Title: LoginTest
+* @Description: 
+* @author: alvin
+* @date 2022年8月1日 上午9:10:41
+*/
+public class LoginTest {
+	
+	public static int a = 1;
+	private Logger logger = Logger.getLogger(LoginTest.class);
+	
+	@Test
+	public void testLoginFailure() throws InterruptedException{
+		logger.info("登录的反向测试用例");
+		Thread.sleep(2000);
+		a++;
+		
+	}
+	
+	@Test
+	public void testLoginSuccess() throws InterruptedException{
+		logger.info("登录的正向测试用例");
+		Thread.sleep(2000);
+		a++;
+	}
+	
+	@AfterTest
+	public void tearDown(){
+		System.out.println("当前LoginTest类里面a的值："+a);
+	}
+	
+
+
+}
