fc2562cbbc4341f2d02392c3fa1b7c7ddc7ba659
alvinsss <6449694@qq.com>
2022-08-17 09:09:17 +0800

    类名要求：Test开头(不含__init__方法)类中用例名：test_开头 都会被收集

---
+++ python_apptest/test_demo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/17 9:07
+# @Author  : alvin
+# @File    : test_demo.py
+# @Software: PyCharm
+
+from add import add
+
+def test_add_four():
+    assert  10== add(1,2,3,4)
