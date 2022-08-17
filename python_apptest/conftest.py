89f20f159fa605b2526c2f87404e3516b065833b
alvinsss <6449694@qq.com>
2022-08-17 11:58:34 +0800

    conftest.py 不需要引入，主动调用。 fixture的查找原则  1、当前文件当中，有没有对应名字的fixture  2、在文件目录下，找conftest.py当中，有没有对应名字的fixture 就近原则！

---
+++ python_apptest/conftest.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/17 11:50
+# @Author  : alvin
+# @File    : conftest.py
+# @Software: PyCharm
+#支持共享。 -- conftest.py 不需要引入，主动调用。
+
+import  pytest
+
+#定义
+@pytest.fixture()
+def setup_teardown():
+    print("===我是测试用例的前置===")
+    #yield 上面前置 后面后置
+    yield
+    print("===我是测试用例的后置===")
+
+@pytest.fixture(scope="class")
+def class_scope():
+    print("===我是类级别测试用例的前置===")
+    yield
+    print("===我是类级别测试用例的后置===")
+
+@pytest.fixture(scope="class")
+def class_scope():
+    pass
+
