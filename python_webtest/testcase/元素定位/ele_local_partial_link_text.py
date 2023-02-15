5b086a07d889ad1ac13a02adb5d9a7cc1047874d
alvinsss <6449694@qq.com>
2023-02-15 09:55:20 +0800

    web自动化增加base_path

--- python_webtest/testcase/元素定位/ele_local_partial_link_text.py
+++ python_webtest/testcase/元素定位/ele_local_partial_link_text.py
@@ -6,14 +6,12 @@
 from selenium import webdriver
 import  os
 import time
+from python_webtest.utils.handle_path import dirver_path
 
-base_path = (os.path.dirname(os.path.dirname(__file__)))
-dirver_path = os.path.join(base_path,"driver_path","chromedriver.exe")
-print(base_path,dirver_path)
-
+print("base_path",dirver_path)
 driver = webdriver.Chrome(executable_path=dirver_path)
 #打开网址
-driver.get("http://novel.hctestedu.com/")
+driver.get("http://****.com/")
 #查找元素 可以通过判断 get_attribute("type") 来进行操作，输入框定位
 login_button = driver.find_element_by_partial_link_text("录")
 login_button.click()
