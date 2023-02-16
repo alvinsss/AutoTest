26ff77577edbb1b857e1b132770487c9ef141072
alvinsss <6449694@qq.com>
2023-02-16 16:22:37 +0800

    完成测试数据编码格式转换初始化，防止后续测试获执行读取测试数据异常

--- python_apitest/testcase/conftest.py
+++ python_apitest/testcase/conftest.py
@@ -9,36 +9,50 @@ from python_apitest.configs.config import NAME_PSW
 from python_apitest.libs.user import User
 from python_apitest.libs.storage import Storage
 from python_apitest.utils.handle_log import log
+from python_apitest.utils.handle_file_encode import read_and_convert
+from python_apitest.utils.handle_path import testData_path
 """
 作用：环境初始化和返回被测试接口实例，方便testcase层直接使用
 ，使用conftest.py进行结合pytest.fixture进行测试环境及变量初始化操作
 """
 #没有返回值的fixture
 @pytest.fixture(scope='session',autouse=True)
-def start_running(convert_encode):
+def start_running(start_init):
     #---自动化测试前的--环境操作
-    log.info('---Alist项目自动化测试开始执行---')
+    log.info('------------------------------')
+    log.info('---Alist项目自动化测试开始执行')
     yield #-yeild后面没有值就是没有返回值
-    log.info('Alist项目自动化测试执行完成，数据清除完成')
+    log.info('---Alist项目自动化测试执行结束')
+    log.info('---Alist项目自动化测试执行完成，数据清除完成')
 
 @pytest.fixture(scope='class')
 def notreturn():#没有yeild或者return  也是没有返回值
     print('没有返回值的fixture')
 
 @pytest.fixture(scope='session')
-def convert_encode():
-    print("\n")
-    log.info('---Alist项目API自动化---')
-    log.info('---Alist项目自动化测试-数据与环境准备--1、批量转换文件编码开始---')
-    # pass
-    log.info('---Alist项目自动化测试-数据与环境准备--批量转换文件编码结束---')
+def start_init(start_init_env,start_init_db,start_init_testdata_convert_encode):
+    log.info('---Alist项目API自动化初始化结束')
 
-    log.info('---Alist项目自动化测试-数据与环境准备--2、数据库垃圾数据处理开始---')
-    # pass
-    log.info('---Alist项目自动化测试-数据与环境准备-- 数据库垃圾数据处理结束---')
-    log.info('---Alist项目自动化测试-数据与环境准备--3、被测服务代码部署检查开始---')
-    # pass
-    log.info('---Alist项目自动化测试-数据与环境准备--被测服务代码部署检查结束---')
+@pytest.fixture(scope='session')
+def start_init_testdata_convert_encode():
+    log.info('---测试数据文件编码转换开始')
+    log.info("--批量转化,测试数据文件:{}进行中...".format(testData_path))
+    read_and_convert(testData_path)
+    log.info('---测试数据文件编码转换结束')
+
+@pytest.fixture(scope='session')
+def start_init_db():
+    log.info('---测试数据库垃圾清理开始')
+    print("标记垃圾数据")
+    log.info('---测试数据库垃圾清理结束')
+
+@pytest.fixture(scope='session')
+def start_init_env():
+    log.info('---Alist项目API自动化初始化开始')
+    log.info('---测试环境部署状态检查开始')
+    print("服务是否可用监测")
+    log.info('---测试环境部署状态检查结束')
+
 #--------登录操作-----------
 #其他的业务层测试代码需要调用这个登录
 #返回值的fixture---直接使用这个函数名--就是代表返回值 -login_init == token
