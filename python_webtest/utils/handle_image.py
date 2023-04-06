5ffafb73aa3f653a95c2895b1208a4e167d70e47
qayunxiao <qawanghailin@gmail.com>
2023-04-06 09:36:51 +0800

    ddddocr图片带干扰线识别

--- python_webtest/utils/handle_image.py
+++ python_webtest/utils/handle_image.py
@@ -5,11 +5,13 @@
 # @Software: PyCharm
 
 import pytesseract
+import ddddocr
 from PIL import Image
+from python_webtest.utils.handle_log import log
 
 class ImageAction():
     '''
-    传入地址 返回验证码
+    传入图片地址 返回图片内容
     '''
     def __init__(self,path):
         self.__path = path
@@ -25,7 +27,7 @@ class ImageAction():
         self.image = Image.open(self.__path)
         if  self.image != None:
             #灰度，去颜色
-            self.image = self.image.convert('L')#全局不需要return
+            self.image = self.image.convert('L')#图像二值化,全局不需要return
         # img.show()#展示灰度之后图片，去颜色
 
     #二值化处理  白的更白，黑的更黑
@@ -39,16 +41,37 @@ class ImageAction():
             else:
                 table.append(1)
         self.image.point(table,'1')
-        # image.show()
+        # self.image.show()
         return self.image
 
+    @property
     def get_captcha_crack(self):
         self.__image_grayscale_deal()
         self.__image_threashoding_method()
         res= pytesseract.image_to_string(self.image)
+        log.info("file {} captcha code:is {} ".format(self.__path,res.split("\n")[0]))
         return res.split("\n")[0]
+
+class Docr1():
+    def __init__(self,path):
+        self.__path = path
+        self.image = None
+        self.ocr1 = ddddocr.DdddOcr()
+
+
+    @property
+    def get_captcha_crackbyd(self):
+        with open(self.__path,'rb') as f:
+            img_bytes = f.read()
+        res = self.ocr1.classification(img_bytes)
+        # print(res,type(res))
+        return res
+
 
 if __name__ == '__main__':
     image = ImageAction("./1.png")
-    res = image.get_captcha_crack()
+    res = image.get_captcha_crack
+    print(res)
+    image = Docr1("./4.jpg")
+    res = image.get_captcha_crackbyd
     print(res)
